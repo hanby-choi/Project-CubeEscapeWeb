ac88183dbf61b7ee2fd1c89c0cf744c04b18e058
flowersayo <flowersayo0811@ewhain.net>
2022-01-12 00:57:27 +0900

    <init> 작업 초기 설정
    디렉토리 생성

---
+++ src/Pages/Entrypage.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @returns 맨 처음 도입부 페이지
+ */
+
+function EntryPage() {
+    return (
+      <div className="App">
+        <h1>Entry page</h1>
+      </div>
+    );
+  }
+  
+  export default EntryPage;
---
+++ src/Pages/MainPage.js
@@ -0,0 +1,11 @@
+
+  export default function Mainpage() {
+    return (
+      <div style={{width : '100%', margin: '0'}}>
+           <h1>main page</h1>
+
+
+        </div>
+    );
+  }
+  
