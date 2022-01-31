ee957191065c5a7d2a9ba27815868a883538e4b5
hanby <hanby.choi@ewhain.net>
2022-01-31 15:55:37 +0900

    <feat> 게임 소개 페이지 다운로드 버튼, 시스템, 플레이 영상 추가
    게임 소개 페이지 기능 추가
    - 다운로드 버튼: 게임 파일은 아직 링크해두지 않음(다운로드 경로 회의 후 결정 - 바로 다운로드 혹은 드라이브로 연결), 버튼 디자인은 임시
    - 시스템: 아이콘 및 텍스트 추가
    - 플레이 영상: 유투브 영상 링크

--- client/src/components/views/IntroPage/IntroPage.js
+++ client/src/components/views/IntroPage/IntroPage.js
@@ -11,7 +11,7 @@ function IntroPage() {
                 <p>
                     정신을 차려보니 정체를 알 수 없는 큐브 공간속에 갇혀 있었다.<br></br>
                     아무것도 기억이 나질 않는다.<br></br>
-                    “여긴 어디…?”
+                    “여긴 어디지…?”
                 </p>
                 
                 <p>이때 큐브의 지배인 C가 나타나 <span style={{color:'red'}}>죽음의 탈출게임</span>을 제안한다.</p>
@@ -24,13 +24,48 @@ function IntroPage() {
                 <p>살아서 나가고 싶다면 정신 똑바로 차려야 해.</p>
             </div>
 
-            <div className="intro_title">SYSTEM</div>
+            <p>
+                <a href="" download><button class="intro_button">Join the Game</button></a>
+                <br></br>
+                <br></br>
+            </p>
 
+            <div className="intro_title">SYSTEM</div>
+            <div style={{textAlign: "center", paddingTop: "10px"}}>
+            <img src="/images/exit_resize.png" style={{width: "10%", height: "10%", marginLeft: "15px"}}></img>
+            <img src="/images/cancel.png" width="5%" height="5%"></img>
+            <img src="/images/mysterious.png" width="10%" height="10%"></img>
+            <img src="/images/cancel.png" width="5%" height="5%"></img>
+            <img src="/images/open-book.png" style={{width: "10%", height: "10%", marginLeft: "15px"}}></img>
+            </div>
+            <div className="intro_body">
+                <span style={{marginRight: "50px"}}>1인칭 시점 방 탈출</span>
+                <span style={{marginRight: "50px"}}>미스터리 퍼즐 어드벤처</span>
+                <span style={{marginLeft:"50px", marginRight: "50px"}}>스토리</span>
+            </div>
+            <br></br>
+            <br></br>
+            
             <div className="intro_title">STAGE RECOMMENDATION</div>
+            <br></br>
+            <br></br>
 
             <div className="intro_title">PLAY VIDIO</div>
+            <br></br>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XaHfOkf_mpI" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
 
 export default IntroPage
+
+/*
+            <table>
+                <tr>
+                <td><img src="/images/exit.png" width="40%" height="40%"></img></td>
+                <td><img src="/images/mysterious.png" width="30%" height="30%"></img></td>
+                <td><img src="/images/open-book.png" width="30%" height="30%"></img></td>
+                </tr>
+            </table>*/
