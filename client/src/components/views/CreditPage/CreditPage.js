910d49977f57c042f5c82439790617813fd5ff14
hanby <hanby.choi@ewhain.net>
2022-02-06 18:41:35 +0900

    <feat> 크레딧 페이지에 icon 출처 추가
    footer의 크레딧 페이지에 icon 이미지 및 출처 추가
    전체적인 css 수정

--- client/src/components/views/CreditPage/CreditPage.js
+++ client/src/components/views/CreditPage/CreditPage.js
@@ -5,23 +5,63 @@ function CreditPage() {
     return (
         <div className="credit">
             <div className="credit_title">CREDIT</div>
-            <p>
-                <div className="credit_subtitle" style={{position: 'absolute', top: '140px'}}> 
+            <div className="credit_container">
+                <div className="credit_subtitle"> 
                     MAIN GAME DESIGN<br></br>
                     SUB GAME DESIGN<br></br>
                     PROGRAMMING<br></br>
                     ART<br></br>
                     PROJECT MANAGER
                 </div>
-                <div className="credit_body" style={{position: 'absolute', top: '140px'}}>
+                <div className="credit_body">
                     김서연<br></br>
                     김지연 이선의 최한비 황주이<br></br>
                     김서연 김지연 이선의 최한비 황주이<br></br>
                     김서연<br></br>
                     김서연
                 </div>
-            </p>
-            
+            </div>
+            <div className="credit_title" style={{fontSize: '150%', paddingTop: '30px'}}>CUBE ESCAPE WEB</div>
+            <div className="credit_container">
+                <div className="credit_subtitle"> 
+                    MAIN GAME DESIGN<br></br>
+                    SUB GAME DESIGN<br></br>
+                    PROGRAMMING<br></br>
+                    ART<br></br>
+                    PROJECT MANAGER
+                </div>
+                <div className="credit_body">
+                    김서연<br></br>
+                    김지연 이선의 최한비 황주이<br></br>
+                    김서연 김지연 이선의 최한비 황주이<br></br>
+                    김서연<br></br>
+                    김서연
+                </div>
+            </div>
+            <div className="credit_title" style={{fontSize: '150%', paddingTop: '30px'}}>WEB ICON</div>
+            <div className="credit_container" style={{marginLeft: "30px"}}>
+                <div className="credit_body">
+                    <div><img src="/images/open-book.png" width="25" height="25" style={{marginRight: "10px"}}></img>
+                    Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik</a> 
+                    <span></span> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+                    <div><img src="/images/exit_resize.png" width="25" height="25" style={{marginRight: "10px"}}></img>
+                    Icons made by <a href="https://www.flaticon.com/authors/bsd" title="bsd"> bsd</a> 
+                    <span></span> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+                    <div><img src="/images/mysterious.png" width="25" height="25" style={{marginRight: "10px"}}></img>
+                    Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware"> Good Ware</a> 
+                    <span></span> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+                    <div><img src="/images/cancel.png" width="25" height="25" style={{marginRight: "10px"}}></img>
+                    Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik</a> 
+                    <span></span> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    
+                    <div><img src="/images/document.png" width="25" height="25" style={{marginRight: "10px"}}></img>
+                    Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart"> smalllikeart</a> 
+                    <span></span> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                </div>
+            </div>            
         </div>
     )
 }
