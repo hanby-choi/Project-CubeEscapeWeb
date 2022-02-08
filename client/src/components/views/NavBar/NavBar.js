dcec435e5f7da4c91c7c998f9df63673d2c1d5db
flowersayo <flowersayo0811@ewhain.net>
2022-02-08 21:09:14 +0900

    <feat> 버튼 위치 변경

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -27,8 +27,8 @@ function NavBar() {
       <span className="menu_left">
           <Menu mode="horizontal" />
         </span>
-  
-        <a href="https://drive.google.com/drive/folders/1uB1wxeukfhtGZqeC5HApO5eKB4MJzkHh?usp=sharing"><Button id="btn" style={{marginTop:'15px', backgroundColor:'black', color:'white', fontFamily: 'Oranienbaum'}}>Get Started</Button></a>
+        
+        <a id="btn" href="https://drive.google.com/drive/folders/1uB1wxeukfhtGZqeC5HApO5eKB4MJzkHh?usp=sharing"><Button  style={{marginTop:'15px', backgroundColor:'black', color:'white'}}>Get Started</Button></a>
 
         <span className="menu_right">
           <LoginMenu mode="horizontal" />
