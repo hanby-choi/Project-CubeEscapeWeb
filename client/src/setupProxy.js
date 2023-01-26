c1969f496215f43498d33d468913572a08c7d814
hanby-choi <hanby.choi@ewhain.net>
2023-01-26 15:26:56 +0900

    fix: change domain address

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://cube-escape.herokuapp.com/',
+            target: 'https://cube-escape-hanby-choi.koyeb.app/',
             changeOrigin: true,
         })
     );
