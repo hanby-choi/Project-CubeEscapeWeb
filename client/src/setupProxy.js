bb9c1a0c8d8e627fee315e3dbe52720a5719bf5b
Hanby Choi <hanby.choi@ewhain.net>
2023-02-03 00:36:32 +0900

    fix: reinstall http-proxy-middleware

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://cube-escape-hanby-choi.koyeb.app',
+        createProxyMiddleware('/api', {
+            target: 'https://cube-escape-hanby-choi.koyeb.app',
             changeOrigin: true,
         })
     );
