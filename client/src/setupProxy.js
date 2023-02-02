75776f40a07a693867568da15946adc9816c26d5
Hanby Choi <hanby.choi@ewhain.net>
2023-02-03 01:41:37 +0900

    url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'https://cube-escape-hanby-choi.koyeb.app',
+            target: 'http://cube-escape-hanby-choi.koyeb.app',
             changeOrigin: true,
         })
     );
