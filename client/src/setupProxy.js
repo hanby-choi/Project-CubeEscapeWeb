2e72c8c412e3c0a63b92c4c73c2b3ea79a595cbd
Hanby Choi <hanby.choi@ewhain.net>
2023-02-02 23:19:17 +0900

    fix: edit proxy again

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'cube-escape-hanby-choi.koyeb.app',
+            target: 'http://cube-escape-hanby-choi.koyeb.app',
             changeOrigin: true,
         })
     );
